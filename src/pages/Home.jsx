42fabeb30ad18377fa1dc2930401010c11ff672f
Morgan Anderson <anderm60@uwosh.edu>
2026-04-14 17:20:37 -0500

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Leafline Digital</h1>
+      <p>Modern, plant-inspired web design for small businesses.</p>
+    </div>
+  )
+}
